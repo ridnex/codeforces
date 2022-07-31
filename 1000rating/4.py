a1411158644ff7bfcd259ff747bc0ea85dba8d3c
ridnex <ridnex28@gmail.com>
2022-07-31 17:38:32 +0600

    codeforces

---
+++ 1000rating/4.py
@@ -0,0 +1,13 @@
+arr = [4, 7, 44, 77, 47, 74, 444, 447, 477, 474, 744, 747, 777, 774]
+str = ""
+k = int(input())
+for i in arr:
+    if k%i==0:
+        str+="b"
+    else:
+        str+="a"
+if "b" in str:
+    print("YES")
+else:
+    print("NO")
+print(arr.reverse())
